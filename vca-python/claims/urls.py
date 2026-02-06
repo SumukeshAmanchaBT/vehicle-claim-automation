14e484270513b3405a1af8fc7c7035bf601b5ebc
Ramakrishnabigtapp <ramakrishna.k@bigtapp.ai>
2026-02-06 17:31:34 +0530

    get apis

--- vca-python/claims/urls.py
+++ vca-python/claims/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import save_fnol, process_claim
+
+from .views import (
+    get_fnol,
+    list_fnol,
+    process_claim,
+    save_fnol,
+)
 
 urlpatterns = [
-    path('save-fnol/', save_fnol),
-    path('process-claim/', process_claim),
+    path("save-fnol/", save_fnol),
+    path("process-claim/", process_claim),
+    path("fnol/", list_fnol),
+    path("fnol/<int:pk>/", get_fnol),
 ]
